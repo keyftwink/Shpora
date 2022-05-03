d3d4ae162d22521ca5fa2a869fb16d224aa48ff1
Ilya <khanakin.ilya10@mail.ru>
2022-05-03 22:27:55 +0300

    Добавление описания к элементам списка

--- app/src/main/java/com/example/shpora/description.java
+++ app/src/main/java/com/example/shpora/description.java
@@ -1,9 +1,14 @@
 package com.example.shpora;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class description extends AppCompatActivity {
 
@@ -15,5 +20,22 @@ public class description extends AppCompatActivity {
         setContentView(R.layout.activity_description);
         Intent intent = getIntent();
         film = intent.getStringExtra("film");
+
+        TextView text = findViewById(R.id.textView);
+        text.setText(getStringFromAssetFile());
+
+    }
+    String getStringFromAssetFile()
+    {
+        AssetManager am = getAssets();
+        try {
+            InputStream is = am.open("descriptionText.txt");
+            String s = String.valueOf(is);
+            is.close();
+            return s;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
